d1f4b09b290e693dbb7c10ddb50233117c1ffe0e
Rishab Choudhary <136588410+RISHABCR7@users.noreply.github.com>
2023-10-10 23:29:34 +0530

    Update RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,22 @@
 import { IMG_CDN_URL } from "../config";
-const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
-  //console.log(restaurant?.data?.data);
-  //const { name, cuisines, cloudinaryImageId, avgRating } = restaurant;
+// const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
+
+const RestaurantCard = ({ restaurant }) => {
+  const{
+  name,
+  cuisines,
+  cloudinaryImageId,
+  costForTwo,
+  areaName,
+  avgRatingString,
+  sla,
+} = restaurant;
   return (
       <div className="w-64 m-8 p-1 hover:shadow-2xl hover:border-gray-900 hover:bg-yellow-50">
       <img src={`${IMG_CDN_URL}${cloudinaryImageId}`} />
       <h2 className="font-bold ">{name}</h2>
       <h3>{cuisines?.join(", ")}</h3>
-      <h4>{`${avgRating} ⭐ `}</h4>
+      <h4>{`${avgRatingString} ⭐ `}</h4>
     </div>
   );
 };
